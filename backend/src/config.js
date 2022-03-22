ea3da24f11689fa51346a54ea8a4fa55750e33e1
shi <bacterianft@gmail.com>
2022-03-22 21:33:02 +0200

    k

--- backend/src/config.js
+++ backend/src/config.js
@@ -65,7 +65,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-03-24T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-03-23T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-03-22T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 500; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x1894f5a167c2b09404027a21f5E5ad3fe7060e82"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
